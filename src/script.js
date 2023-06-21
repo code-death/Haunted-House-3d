98c00ad3961f82b6b2b08cd074f78e412379c1f2
code-death <poorvanshkavta938@gmail.com>
2023-06-21 16:57:55 +0530

    Camera position updated

--- src/script.js
+++ src/script.js
@@ -316,8 +316,8 @@ window.addEventListener('resize', () =>
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 4
-camera.position.y = 2
-camera.position.z = 5
+camera.position.y = 3
+camera.position.z = 8
 scene.add(camera)
 
 // Controls
